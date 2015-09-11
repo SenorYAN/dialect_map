5b6d9cd4a8366d195f559fc16b5461cc33570b7c
maoyan <zhouqimaoyan@163.com>
2015-09-11 21:33:13 +0800

    9.11-3

---
+++ jiangsu/js/data.js
@@ -0,0 +1,20 @@
+$('#JiangsuMap').SVGMap({
+		mapName: 'jiangsu',
+		stateData: {
+		//南京
+		"nanjing":{"value":"0.3","index":"1","stateInitColor":"7"},
+		"jiangning":{"value":"0.3","index":"2","stateInitColor":"7"},
+		"pukou":{"value":"0.3","index":"3","stateInitColor":"7"},
+		"lishui":{"value":"0.5","index":"4","stateInitColor":"5"},
+		"gaochun":{"value":"0.6","index":"5","stateInitColor":"6"},
+		"liuhe":{"value":"0.2","index":"6","stateInitColor":"8"},
+		//无锡
+		"wuxi":{"value":"0.9","index":"7","stateInitColor":"1"},
+		"jiangyin":{"value":"0.8","index":"8","stateInitColor":"2"},
+		"yixing":{"value":"0.8","index":"9","stateInitColor":"2"},
+		"xishan":{"value":"0.9","index":"10","stateInitColor":"1"},
+		//徐州
+		"xuzhou":{"value":"-1","index":"11","stateInitColor":"15"},
+		"fengxian":{"value":"-0.9","index":"12","stateInitColor":"14"},
+					}
+	});
---
+++ jiangsu/js/demo1.js
@@ -0,0 +1,41 @@
+(function() {
+	var triggerBttn2 = document.getElementById( 'trigger-overlay2' ),
+		overlay2 = document.querySelector( 'div#overlay2' ),
+		closeBttn2 = overlay2.querySelector( 'button#btn2' );
+		transEndEventNames = {
+			'WebkitTransition': 'webkitTransitionEnd',
+			'MozTransition': 'transitionend',
+			'OTransition': 'oTransitionEnd',
+			'msTransition': 'MSTransitionEnd',
+			'transition': 'transitionend'
+		},
+		transEndEventName = transEndEventNames[ Modernizr.prefixed( 'transition' ) ],
+		support = { transitions : Modernizr.csstransitions };
+
+
+	function toggleOverlay2() {
+		if( classie.has( overlay2, 'open' ) ) {
+			classie.remove( overlay2, 'open' );
+			classie.add( overlay2, 'close' );
+			var onEndTransitionFn = function( ev ) {
+				if( support.transitions ) {
+					if( ev.propertyName !== 'visibility' ) return;
+					this.removeEventListener( transEndEventName, onEndTransitionFn );
+				}
+				classie.remove( overlay2, 'close' );
+			};
+			if( support.transitions ) {
+				overlay2.addEventListener( transEndEventName, onEndTransitionFn );
+			}
+			else {
+				onEndTransitionFn();
+			}
+		}
+		else if( !classie.has( overlay2, 'close' ) ) {
+			classie.add( overlay2, 'open' );
+		}
+	}
+
+	triggerBttn2.addEventListener( 'click', toggleOverlay2 );
+	closeBttn2.addEventListener( 'click', toggleOverlay2 );
+})();
